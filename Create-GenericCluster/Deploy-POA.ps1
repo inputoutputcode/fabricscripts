812fdbf84704ad80051b0a6af5361552ef7abceb
Christian Pappert <chrpap@microsoft.com>
2020-08-05 14:45:24 -0700

    best practice anaylzer config plus poa

---
+++ Create-GenericCluster/Deploy-POA.ps1
@@ -0,0 +1,23 @@
+﻿
+$subscriptionId = "13ad2c84-84fa-4798-ad71-e70c07af873f"
+$appPackageUrl = "https://chrpapblobs.blob.core.windows.net/binaries/PatchOrchestrationApplication_v1.3.2.sfpkg?sp=r&st=2020-08-05T20:31:23Z&se=2020-08-06T04:31:23Z&spr=https&sv=2019-12-12&sr=b&sig=k5MfiXa4Qqf42n3PuYaUkSpzQLxycQSvdFsIZal624Y%3D"
+$deploymentName = "chrpap051400"
+$serviceFabricClusterName = $deploymentName + "-servicefabric"
+$resourceGroup = $deploymentName + "-group"
+$armTemplate = ".\Templates\patch-orchestration-application.json"
+
+Try {
+  Select-AzSubscription -SubscriptionId $subscriptionId -ErrorAction Stop
+} Catch {
+    Login-AzAccount
+    Set-AzContext -SubscriptionId $subscriptionId
+}
+
+$armParameter = @{}
+$armParameter.Add("appPackageUrl", $appPackageUrl)
+$armParameter.Add("clusterName", $serviceFabricClusterName)
+
+Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterObject $armParameter -Verbose -ErrorAction Stop
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterObject $armParameter -Verbose -Mode Complete -Force
+
--- Create-GenericCluster/Update-ClusterDeployment.ps1
+++ Create-GenericCluster/Update-ClusterDeployment.ps1
@@ -1,5 +1,5 @@
 ﻿
-$armTemplate = ".\Templates\Vortex-LoadTest-Cluster.json"
+$armTemplate = ".\Templates\patch-orchestration-application.json"
 $currentExecutionPath = "D:\Code\inputoutputcode\FabricMonkey\Create-GenericCluster"
 $subscriptionId = "13ad2c84-84fa-4798-ad71-e70c07af873f"
 $clusterVersion = "7.1.409.9590"
@@ -9,7 +9,7 @@ $generalPassword = "nZ549Ux2MnW6srTvOZsq"
 cd $currentExecutionPath
 Enable-AzureRmAlias
 
-$deploymentName = "chrpap011951"
+$deploymentName = "chrpap050920"
 
 # Define dynamic parameters
 $certificateName = $deploymentName + "-cert"
@@ -19,9 +19,9 @@ $keyVaultName = $deploymentName + "-keyvault"
 $serviceFabricClusterName = $deploymentName + "-servicefabric"
 $serviceFabricClusterDns = $serviceFabricClusterName + "." + $azureRegion + ".cloudapp.azure.com"
 
-$sourceVault = "/subscriptions/13ad2c84-84fa-4798-ad71-e70c07af873f/resourceGroups/chrpap011951-group/providers/Microsoft.KeyVault/vaults/chrpap011951-keyvault"
-$certificateURL = "https://chrpap011951-keyvault.vault.azure.net:443/secrets/chrpap011951-cert/7939748ff14e4ee5bb9ba11e90663609"
-$certificateThumbprint = "EC9E3DD93941A3B4EBCA4BE039B30D963A5EED92"
+$sourceVault = "/subscriptions/13ad2c84-84fa-4798-ad71-e70c07af873f/resourceGroups/chrpap311337-group/providers/Microsoft.KeyVault/vaults/chrpap311337-keyvault"
+$certificateURL = "https://chrpap311337-keyvault.vault.azure.net/secrets/chrpap311337-cert/c5cded107eb941a2832b829ecb8301ba"
+$certificateThumbprint = "CAB8D58228BE2247038206DE2564E141CE519D7E"
 
 ## Deploy Azure Service Fabric Cluster
 $armParameter = @{}
@@ -36,5 +36,5 @@ $armParameter.Add("certificateThumbprint", $certificateThumbprint)
 
 Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterObject $armParameter -Verbose -ErrorAction Stop
 
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterObject $armParameter -Verbose -Mode Complete -Force
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup - -TemplateFile $armTemplate -TemplateParameterObject $armParameter -Verbose -Mode Complete -Force
 

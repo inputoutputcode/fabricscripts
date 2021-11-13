7b261ff4d212e09d50f38bf5804059a4e4cfa7e5
Christian Pappert <3459390+inputoutputcode@users.noreply.github.com>
2021-11-12 20:07:59 -0800

    update cert, add second node type

--- Create-GenericCluster/Create-GenericCluster.ps1
+++ Create-GenericCluster/Create-GenericCluster.ps1
@@ -1,7 +1,7 @@
 ﻿## Declare parameters
 $armTemplate = ".\Templates\Vortex-LoadTest-Cluster.json" # 
 $currentExecutionPath = "D:\Code\FabricMonkey\FabricScripts\Create-GenericCluster"
-$clusterVersion = "8.0.514.9590"
+$clusterVersion = "8.2.1235.9590"
 
 ## Fixed parameters
 $subscriptionId = "13ad2c84-84fa-4798-ad71-e70c07af873f" 
---
+++ Scripts/POA/Deploy-FO.ps1
@@ -0,0 +1,12 @@
+﻿
+
+$resourceGroup = "chrpap271653-servicefabric"
+$armTemplate = "service-fabric-observer.json"
+$armTemplateParameters = "service-fabric-observe.v3.1.20.parameters.json"
+
+cd "C:\Users\Christian\Documents\Customer\adVANce\PowerShell\PatchOrchestrationApplication"
+
+Connect-AzureRmAccount
+
+New-AzureRmResourceGroupDeployment -Name "deploy-service-fabric-observer" -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterFile $armTemplateParameters -Verbose -Mode Incremental
+
